c4271c0598917c6967267fa3c5891d5407f18cbc
iDesxtop <muhammadalvinza@gmail.com>
2026-01-06 18:02:13 +0700

    fixed id on rps

--- Benih/RPS.js
+++ Benih/RPS.js
@@ -1,55 +1,55 @@
-db.rps.insertMany([
+db.RPS.insertMany([
   // --- BAYU (Ref ID: ...ea6) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") }, // Sistem Cerdas
-  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") }, // Comp Vision
-  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") }, // NoSQL
-  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") }, // Statistika
+  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") }, // Sistem Cerdas
+  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") }, // Comp Vision
+  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") }, // NoSQL
+  { "user_id": ObjectId("695b8c2b540a02b5137daea6"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") }, // Statistika
 
   // --- DANES (Ref ID: ...eb1) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c30540a02b5137daeb1"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- FAISAL (Ref ID: ...eb7) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c31540a02b5137daeb7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- ICHSAN (Ref ID: ...ebe) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c33540a02b5137daebe"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- MARCO (Ref ID: ...ec7) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c34540a02b5137daec7"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- RAUF (Ref ID: ...ed0) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c36540a02b5137daed0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- YOGA (Ref ID: ...ed8) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c37540a02b5137daed8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- YUSUF (Ref ID: ...ee0) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") },
+  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c39540a02b5137daee0"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") },
 
   // --- ZAKI (Ref ID: ...ee8) mengambil 4 Matkul ---
-  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31445") },
-  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31446") },
-  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31447") },
-  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695b8b477afa3aa72dc31448") }
+  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5") },
+  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a6") },
+  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a7") },
+  { "user_id": ObjectId("695b8c3b540a02b5137daee8"), "matkul_id": ObjectId("695bd0e37563f8ba7000a9a8") }
 ]);
