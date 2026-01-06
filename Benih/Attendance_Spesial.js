ad52801a7f8e9b60f3f91431fecd67fe4d1cbc49
iDesxtop <muhammadalvinza@gmail.com>
2026-01-06 14:33:36 +0700

    added new seed

---
+++ Benih/Attendance_Spesial.js
@@ -0,0 +1,20 @@
+db.Attendance_Spesial.insertMany([
+  {
+    "_id": ObjectId("696cdef27563f8ba7000c221"),
+    "user_id": ObjectId("695b8c2b540a02b5137daea6"), // Nama: Bayu
+    "spesial_id": ObjectId("696cdef27563f8ba7000b111"), // Referensi ke Kelas_Spesial Siscer
+    "timestamp": "2026-02-06 09:55:10"
+  },
+  {
+    "_id": ObjectId("696cdef27563f8ba7000c222"),
+    "user_id": ObjectId("695b8c30540a02b5137daeb1"), // Nama: Danes
+    "spesial_id": ObjectId("696cdef27563f8ba7000b111"),
+    "timestamp": "2026-02-06 10:05:45"
+  },
+  {
+    "_id": ObjectId("696cdef27563f8ba7000c223"),
+    "user_id": ObjectId("695b8c31540a02b5137daeb7"), // Nama: Faisal
+    "spesial_id": ObjectId("696cdef27563f8ba7000b111"),
+    "timestamp": "2026-02-06 09:58:30"
+  }
+]);
---
+++ Benih/Kelas_Spesial.js
@@ -0,0 +1,9 @@
+db.Kelas_Spesial.insertOne({
+  "_id": ObjectId("696cdef27563f8ba7000b111"),
+  "matkul_id": ObjectId("695bd0e37563f8ba7000a9a5"), // ID Matkul: Sistem Cerdas
+  "class_id": ObjectId("694f3f1b825e7bcbbe801d14"), // ID Kelas: 201 (Gedung FPMIPA B)
+  "pertemuan": 2,
+  "jam_awal": "10:00",
+  "jam_akhir": "12:15",
+  "tanggal_kelas": ISODate("2026-02-06T10:00:00.000Z")
+});
