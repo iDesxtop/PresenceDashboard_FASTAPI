8ca2d924b8263acb66217ccb3e84c2c3ab11a7e4
iDesxtop <muhammadalvinza@gmail.com>
2026-01-06 23:11:24 +0700

    added user to seed

---
+++ Benih/Users.js
@@ -0,0 +1,56 @@
+db.Users.insertMany([
+  {
+    "_id": ObjectId("695b8c2b540a02b5137daea6"),
+    "name": "Bayu",
+    "nim": "2304192",
+    "created_at": ISODate("2026-01-05T17:02:19.793Z")
+  },
+  {
+    "_id": ObjectId("695b8c30540a02b5137daeb1"),
+    "name": "Danes",
+    "nim": "2358104",
+    "created_at": ISODate("2026-01-05T17:02:24.476Z")
+  },
+  {
+    "_id": ObjectId("695b8c31540a02b5137daeb7"),
+    "name": "Faisal",
+    "nim": "2319283",
+    "created_at": ISODate("2026-01-05T17:02:25.638Z")
+  },
+  {
+    "_id": ObjectId("695b8c33540a02b5137daebe"),
+    "name": "Ichsan",
+    "nim": "2390124",
+    "created_at": ISODate("2026-01-05T17:02:27.066Z")
+  },
+  {
+    "_id": ObjectId("695b8c34540a02b5137daec7"),
+    "name": "Marco",
+    "nim": "2374610",
+    "created_at": ISODate("2026-01-05T17:02:28.840Z")
+  },
+  {
+    "_id": ObjectId("695b8c36540a02b5137daed0"),
+    "name": "Rauf",
+    "nim": "2325381",
+    "created_at": ISODate("2026-01-05T17:02:30.483Z")
+  },
+  {
+    "_id": ObjectId("695b8c37540a02b5137daed8"),
+    "name": "Yoga",
+    "nim": "2309472",
+    "created_at": ISODate("2026-01-05T17:02:31.969Z")
+  },
+  {
+    "_id": ObjectId("695b8c39540a02b5137daee0"),
+    "name": "Yusuf",
+    "nim": "2361905",
+    "created_at": ISODate("2026-01-05T17:02:33.526Z")
+  },
+  {
+    "_id": ObjectId("695b8c3b540a02b5137daee8"),
+    "name": "Zaki",
+    "nim": "2382019",
+    "created_at": ISODate("2026-01-05T17:02:35.077Z")
+  }
+]);
